8614591b9de6feab4fac52aa50e29f42bb988148
bread-bread <p.reshetnev@yandex.ru>
2017-07-03 11:53:34 +0300

    animations_with_bags

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,77 @@
-(function() {
-  'use strict';
+////////////////////////////////////////////////////
+(function(){
+  let flipper = document.querySelector('.flipper-wrap'),
+      button = document.querySelector('.auth-btn');
+  if (button) {
+  button.addEventListener('click', function(e){
+    e.preventDefault();
+    flipper.classList.toggle('clicked');
+  })
+  }
+}());
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
+(function(){
+  let triggerBtn = document.querySelector('.nav-trigger');
+
+  if (triggerBtn) {
+  triggerBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    triggerBtn.classList.toggle('nav-trigger_active');
+  })
+  }
+}());
+
+///////////////////////////////////////////////////////////
+var parallax = (function(){
+  var bg = document.querySelector('.first-screen__bg-wrap'),
+      user = document.querySelector('.user'),
+      sectionPic = document.querySelector('.section-pic');
+
+  return {
+    move: function(block, windowScroll, strafeAmount) {
+      var strafe = windowScroll / -strafeAmount + '%';
+      var transformStr = 'translate3d(0, '+ strafe +', 0)';
+
+      var style = block.style;
+
+      style.transform = transformStr;
+      style.webkitTransform = transformStr;
+
+    },
+    init: function(wScroll) {
+      this.move(bg, wScroll, 55);
+      this.move(sectionPic, wScroll, 25);
+      this.move(user, wScroll, 3);
+    }
+  }
+
+}());
+
+window.onscroll = function(){
+  var wScroll = window.pageYOffset;
+  parallax.init(wScroll);
+};
+////////////////////////////////////////////////////
+
+var helloParallaxCont = document.querySelector('.hello-bg'),
+    layers = helloParallaxCont.children;
+
+var moveLayers = function(e) {
+  var initialX = (window.innerWidth / 2) - e.pageX;
+  var initialY = (window.innerHeight / 2) - e.pageY;
+
+  console.log(initialX, initialY);
+
+  [].slice.call(layers).forEach(function(layer, i){
+    var divider = i/100,
+        positionX = initialX * divider,
+        positionY = initialY * divider,
+        bottomPosition = (window.innerHeight / 2) * divider,
+        transformStr = 'translate('+ positionX +'px,'+ positionY +'px)',
+        image = layer.firstElementChild;
+
+    layer.style.transform = transformStr;
+    image.style.bottom = -bottomPosition + 'px';
+  })
+}
+window.addEventListener('mousemove', moveLayers);
