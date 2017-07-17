89546569c87989f55b65e4fead9a36c9e13fcd26
bread-bread <p.reshetnev@yandex.ru>
2017-07-17 20:30:44 +0300

    correction_3

--- source/js/modules/blog_menu.js
+++ source/js/modules/blog_menu.js
@@ -1,17 +1,17 @@
 export default () => {
     $('.sidebar__trigger').on('click', function(e){
         e.preventDefault();
-        $('.sidebar').animate({
-            'width': '80%'
-        });
+        $('.sidebar').css(
+            'width', '80%'
+        );
         $('.sidebar').addClass('active');
     });
     $(document).on('click', function(e){
         var $this = $(e.target);
         if(!$this.closest('.sidebar').length && $('.sidebar').hasClass('active')) {
-            $('.sidebar').animate({
-                'width': '0'
-            });
+            $('.sidebar').css(
+                'width', '0'
+            );
             $('.sidebar').removeClass('active');
         }
     })
